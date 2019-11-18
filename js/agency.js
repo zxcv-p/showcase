be3d522b1acea75c83cc6a3dcd649a1a4aa4cd95
Pierre <pierre.binauld@gmail.com>
2019-11-18 18:27:26 +0100

    Init bootstrap4 migration

--- js/agency.js
+++ js/agency.js
@@ -17,12 +17,12 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    $('.navbar-toggler:visible').click();
 });
 
 $('div.modal').on('show.bs.modal', function() {
